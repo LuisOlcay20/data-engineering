c609af65c38609f130ab5578d2aee4715f8b88bc
Luis Olcay <luis.olcay@uc.cl>
2024-03-10 22:11:48 -0300

    Remove ny_taxi_postgres_data from tracking and dockerize ingest_data script

--- 01-docker-terraform/docker_sql/ingest_data.py
+++ 01-docker-terraform/docker_sql/ingest_data.py
@@ -1,6 +1,8 @@
+import os
+import argparse
+from time import time
 import pandas as pd
 import pyarrow.parquet as pq
-import argparse
 from sqlalchemy import create_engine
 import requests
 
@@ -9,34 +11,55 @@ def download_file(url, filename):
     response.raise_for_status()  
     with open(filename, 'wb') as f:
         f.write(response.content)
+
 def main(params):
     user = params.user
     password = params.password
-    host = params.host
-    port = params.port
+    host = params.host 
+    port = params.port 
     db = params.db
     table_name = params.table_name
     url = params.url
-    parquet_name = 'yellow_tripdata_2021-01.parquet'
+    
+   
+    if url.endswith('.parquet'):
+        parquet_file = 'output.parquet'
+    else:
+        raise ValueError("URL must point to a Parquet file")
 
-    download_file(url, parquet_name)
-
-    df = pq.read_table(parquet_name).to_pandas()
+   
+    download_file(url, parquet_file)
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
+   
+    parquet_iter = pq.ParquetFile(parquet_file).iter_batches(batch_size=100000)
+    batch = next(parquet_iter)
+    df = pd.DataFrame(batch.to_pandas())
+
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
-    chunk_size = 100000
-    total_chunks = len(df) // chunk_size + (1 if len(df) % chunk_size else 0)
+    df.to_sql(name=table_name, con=engine, if_exists='append')
 
-    for i, start in enumerate(range(0, len(df), chunk_size)):
-        chunk = df[start:start + chunk_size]
-        chunk.to_sql(name=table_name, con=engine, if_exists='append', index=False)
-        print(f"Chunk {i+1}/{total_chunks} procesado y cargado en la base de datos")
+    while True:
+        try:
+            t_start = time()
+            
+            batch = next(parquet_iter)
+            df = pd.DataFrame(batch.to_pandas())
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('Inserted another chunk, took %.3f second' % (t_end - t_start))
+
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest data to PG')
+    parser = argparse.ArgumentParser(description='Ingest Parquet data to Postgres')
 
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
@@ -44,7 +67,9 @@ if __name__ == '__main__':
     parser.add_argument('--port', required=True, help='port for postgres')
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--url', required=True, help='URL of the Parquet file')
 
     args = parser.parse_args()
+
     main(args)
+
